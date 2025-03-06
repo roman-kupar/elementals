14762f1793df2c81916546127b53e65ff89db5e9
Roman <romko2005@gmail.com>
2025-03-06 13:00:54 +0100

    Added Music. Updated Menu structure
    TODO done

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,5 +1,9 @@
 extends Control
 
+@onready var levels_menu: Control = $LevelsMenu
+@onready var main_menu: VBoxContainer = $VBoxContainer
+@onready var title: Label = $Label
+@onready var back: Button = $Back
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -10,13 +14,21 @@ func _process(delta: float) -> void:
 	pass
 
 func _on_start_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/levels/level_1.tscn")
-	#TODO
-	#Change that, to load the level, based on last unlocked level
+	var last_level = SaveSystem.last_level 
+	var level_path = "res://scenes/levels/level_" + str(last_level) + ".tscn"
+	get_tree().change_scene_to_file(level_path)
 	
-
 func _on_levels_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/UI/levels.tscn")
+	main_menu.hide()
+	title.hide()
+	levels_menu.show()
+	back.show()
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
+
+func _on_back_pressed() -> void:
+	main_menu.show()
+	title.show()
+	levels_menu.hide()
+	back.hide()
