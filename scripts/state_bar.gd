b59526674d06c52f0b3fc94f797e38c3f21c499a
wivest <120594169+wivest@users.noreply.github.com>
2025-03-06 20:24:52 +0100

    display order

--- scripts/state_bar.gd
+++ scripts/state_bar.gd
@@ -2,17 +2,46 @@ class_name StateBar
 extends HBoxContainer
 
 @export var order: Array[Player.Element]
+@export var ground: CompressedTexture2D
+@export var water: CompressedTexture2D
+@export var air: CompressedTexture2D
+@export var none: CompressedTexture2D
 
 var current: int = 0
+var elements: Array[TextureRect]
+
+func _ready():
+	for element in order:
+		var texture = match_texture(element)
+		var rect = TextureRect.new()
+		rect.texture = texture
+		add_child(rect)
+		elements.append(rect)
+
+
+func match_texture(element: Player.Element) -> CompressedTexture2D:
+	match element:
+		Player.Element.GROUND:
+			return ground
+		Player.Element.WATER:
+			return water
+		Player.Element.AIR:
+			return air
+	return none
 
 func validate(next: Player.Element) -> bool:
 	if current >= order.size():
 		return false
 
 	if order[current] == Player.Element.NONE:
+		fade_element(current)
 		current += 1
 		return true
 	if next == order[current]:
+		fade_element(current)
 		current += 1
 		return true
 	return false
+
+func fade_element(i: int):
+	elements[i].modulate = Color.GRAY
